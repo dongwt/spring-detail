4518b11af8572ce4018f49e4995a98b372ef8a7f
dongwt <wentao.dong@ele.me>
2018-04-15 15:32:29 +0800

    spring事务处理

--- src/main/java/com/dongwt/spring/tansaction/service/TransactionService.java
+++ src/main/java/com/dongwt/spring/tansaction/service/TransactionService.java
@@ -3,8 +3,10 @@ package com.dongwt.spring.tansaction.service;
 
 import com.dongwt.spring.model.Consumer;
 import com.dongwt.spring.tansaction.mapper.ConsumerMapper;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -16,13 +18,26 @@ public class TransactionService {
     @Autowired
     private ConsumerMapper consumerMapper;
 
-    @Transactional
+
+    @Transactional(propagation = Propagation.REQUIRED)
     public void add(){
         Consumer consumer = new Consumer();
         consumer.setName("tom");
         consumer.setPhone("110");
         consumer.setAge(1);
         consumer.setSex(1);
+        consumerMapper.add(consumer);
+        ((TransactionService)AopContext.currentProxy()).add1();
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void add1(){
+        Consumer consumer = new Consumer();
+        consumer.setName("tom2");
+        consumer.setPhone("120");
+        consumer.setAge(2);
+        consumer.setSex(2);
         consumerMapper.add(consumer);
         int a = 1/0;
     }
